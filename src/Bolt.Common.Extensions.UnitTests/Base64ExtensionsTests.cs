fed82a6f867176d33b67dc041f73c7ac42e6bff6
Mohammad Ruhul Amin <mruhul@gmail.com>
2021-08-27 23:57:02 +1000

    Base64 encode and decode extension methods

---
+++ src/Bolt.Common.Extensions.UnitTests/Base64ExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Bolt.Common.Extensions.UnitTests
+{
+    public class Base64ExtensionsTests
+    {
+        [Theory]
+        [InlineData("Hello", "SGVsbG8=")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        public void EncodeBas64Test(string givenUtf8, string expected)
+        {
+            var gotEncoded = givenUtf8.Base64Encode();
+            gotEncoded.ShouldBe(expected);
+        }
+
+        [Theory]
+        [InlineData("SGVsbG8=", "Hello")]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        public void DecodeBas64Test(string givenBase64, string expected)
+        {
+            var gotEncoded = givenBase64.Base64Decode();
+            gotEncoded.ShouldBe(expected);
+        }
+    }
+}
---
+++ src/Bolt.Common.Extensions/Base64Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace Bolt.Common.Extensions
+{
+    public static class Base64Extensions
+    {
+        /// <summary>
+        /// Convert a UTF8 string to Base64 string
+        /// </summary>
+        /// <param name="utf8"></param>
+        /// <returns></returns>
+        public static string Base64Encode(this string utf8)
+            => string.IsNullOrWhiteSpace(utf8)
+                ? utf8
+                : Convert.ToBase64String(Encoding.UTF8.GetBytes(utf8));
+
+        /// <summary>
+        /// Convert a Base64 string to UTF8 string
+        /// </summary>
+        /// <param name="base64"></param>
+        /// <returns></returns>
+        public static string Base64Decode(this string base64)
+            => string.IsNullOrWhiteSpace(base64) 
+                ? base64 
+                : Encoding.UTF8.GetString(Convert.FromBase64String(base64));
+    }
+}

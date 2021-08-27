032e9bffeb8d03902353916b7ea0ec43b393a4d4
Mohammad Ruhul Amin <mruhul@gmail.com>
2021-08-27 23:59:23 +1000

    Moe enumerable extensions

---
+++ src/Bolt.Common.Extensions.UnitTests/EnumerableExtensionsTests.cs
@@ -0,0 +1,139 @@
+﻿using Shouldly;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Bolt.Common.Extensions.UnitTests
+{
+    public class EnumerableExtensionsTests
+    {
+        [Fact]
+        public void IsEmptyReturnsTrueWhenNull()
+        {
+            IEnumerable<string> collection = null;
+
+            collection.IsEmpty().ShouldBe(true);
+        }
+
+        [Fact]
+        public void IsEmptyReturnsTrueWhenEmpty()
+        {
+            var collection = new List<string>();
+
+            collection.IsEmpty().ShouldBe(true);
+        }
+
+        [Fact]
+        public void IsEmptyReturnsFalseWhenNotEmpty()
+        {
+            var collection = new List<string> { "1" };
+
+            collection.IsEmpty().ShouldBe(false);
+        }
+
+
+        [Fact]
+        public void HasItemReturnsFalseWhenNull()
+        {
+            IEnumerable<string> collection = null;
+
+            collection.HasItem().ShouldBe(false);
+        }
+
+        [Fact]
+        public void HasItemReturnsFalseWhenEmpty()
+        {
+            var collection = new List<string>();
+
+            collection.HasItem().ShouldBe(false);
+        }
+
+        [Fact]
+        public void HasItemReturnsTrueWhenNotEmpty()
+        {
+            var collection = new List<string> { "1" };
+
+            collection.HasItem().ShouldBe(true);
+        }
+
+        [Fact]
+        public void ContainsIgnoreCaseReturnFalseWhenCollectionIsNull()
+        {
+            List<string> collection = null;
+
+            collection.ContainsIgnoreCase("test").ShouldBe(false);
+        }
+
+        [Fact]
+        public void ContainsIgnoreCaseReturnFalseWhenValueIsNull()
+        {
+            List<string> collection = new List<string>
+            {
+                "test"
+            };
+
+            collection.ContainsIgnoreCase(null).ShouldBe(false);
+        }
+
+
+
+        [Fact]
+        public void ContainsIgnoreCaseReturnFalseWhenValueIsEmpty()
+        {
+            List<string> collection = new List<string>
+            {
+                "test"
+            };
+
+            collection.ContainsIgnoreCase(string.Empty).ShouldBe(false);
+        }
+
+        [Fact]
+        public void ContainsIgnoreCaseReturnFalseWhenCollectionDoesntContainsTheValue()
+        {
+            List<string> collection = new List<string>
+            {
+                "test-1"
+            };
+
+            collection.ContainsIgnoreCase("test").ShouldBe(false);
+        }
+
+        [Fact]
+        public void ContainsIgnoreCaseReturnTrueWhenCollectionContainsTheValueWithSameCase()
+        {
+            List<string> collection = new List<string>
+            {
+                "test-1",
+                "test"
+            };
+
+            collection.ContainsIgnoreCase("test").ShouldBe(true);
+        }
+
+
+        [Fact]
+        public void ContainsIgnoreCaseReturnTrueWhenCollectionContainsTheValueWithDifferentCase()
+        {
+            List<string> collection = new List<string>
+            {
+                "test-1",
+                "TEst"
+            };
+
+            collection.ContainsIgnoreCase("test").ShouldBe(true);
+        }
+
+        [Fact]
+        public void ContainsAnyIgnoreCaseReturnTrueWhenOneItemMatch()
+        {
+            var collection = new List<string>
+            {
+                "one",
+                "two",
+                "three"
+            };
+
+            collection.ContainsAnyIgnoreCase("Two", "three").ShouldBeTrue();
+        }
+    }
+}
--- src/Bolt.Common.Extensions/EnumerableExtensions.cs
+++ src/Bolt.Common.Extensions/EnumerableExtensions.cs
@@ -7,30 +7,62 @@ namespace Bolt.Common.Extensions
 {
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Return whether an IEnumerable is null or empty
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
         [DebuggerStepThrough]
         public static bool IsEmpty<T>(this IEnumerable<T> source)
         {
             return source == null || !source.Any();
         }
 
+        /// <summary>
+        /// Return whether an IEnumerable has any item or not null safe way
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
         [DebuggerStepThrough]
         public static bool HasItem<T>(this IEnumerable<T> source)
         {
             return source != null && source.Any();
         }
 
+        /// <summary>
+        /// Add items end of IEnumerable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="items"></param>
+        /// <returns></returns>
         [DebuggerStepThrough]
         public static IEnumerable<T> Append<T>(this IEnumerable<T> source, params T[] items)
         {
             return items == null ? source : source.Concat(items);
         }
 
+        /// <summary>
+        /// Add items beginning of Ienumerable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="items"></param>
+        /// <returns></returns>
         [DebuggerStepThrough]
         public static IEnumerable<T> Prepend<T>(this IEnumerable<T> source, params T[] items)
         {
             return items == null ? source : items.Concat(source);
         }
 
+        /// <summary>
+        /// Perform an action for each item
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="action"></param>
         [DebuggerStepThrough]
         public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
@@ -38,6 +70,57 @@ namespace Bolt.Common.Extensions
             {
                 action.Invoke(item);
             }
+        }
+
+        /// <summary>
+        /// Check whether a string exists in a collection of string in case insensitive way
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static bool ContainsIgnoreCase(this IEnumerable<string> source, string value)
+        {
+            if (source == null) return false;
+            
+            if (value == null) return false;
+            
+            foreach(var item in source)
+            {
+                if(string.Equals(item, value, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Check whether any of the supplied value exists in collection in case insensitive way
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static bool ContainsAnyIgnoreCase(this IEnumerable<string> source, params string[] value)
+        {
+            if (source == null) return false;
+
+            if (value == null) return false;
+
+            foreach(var valueToFind in value)
+            {
+                foreach (var item in source)
+                {
+                    if (string.Equals(item, valueToFind, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }

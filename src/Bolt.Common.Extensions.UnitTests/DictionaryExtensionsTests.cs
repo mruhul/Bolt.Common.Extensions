5dc26e0301a14ee769bebb1f7d530b6d2c383fc9
Mohammad Ruhul Amin <mruhul@gmail.com>
2021-08-28 14:58:39 +1000

    Add new method for dictionary to merge using case insensitive key
    Add a new method for dictionary to append another dictionary to existing dictionary

--- src/Bolt.Common.Extensions.UnitTests/DictionaryExtensionsTests.cs
+++ src/Bolt.Common.Extensions.UnitTests/DictionaryExtensionsTests.cs
@@ -40,5 +40,130 @@ namespace Bolt.Common.Extensions.UnitTests
             result["three"].ShouldBe("3");
             result["four"].ShouldBe("4");
         }
+
+        [Fact]
+        public void MergeTestsWhenIgnoreExistingIsTru()
+        {
+            var baseStore = new Dictionary<string, string>
+            {
+                ["one"] = "1",
+                ["two"] = "2",
+                ["three"] = "3"
+            };
+
+            var newStore = new Dictionary<string, string>
+            {
+                ["four"] = "4",
+                ["two"] = "-2-"
+            };
+
+            var result = baseStore.Merge(newStore, ignoreWhenAlreadyExists: true);
+
+            result.Count.ShouldBe(4);
+            result["one"].ShouldBe("1");
+            result["two"].ShouldBe("2");
+            result["three"].ShouldBe("3");
+            result["four"].ShouldBe("4");
+        }
+
+        [Fact]
+        public void MergeIgnoreCaseTests()
+        {
+            var baseStore = new Dictionary<string, string>
+            {
+                ["one"] = "1",
+                ["two"] = "2",
+                ["three"] = "3"
+            };
+
+            var newStore = new Dictionary<string, string>
+            {
+                ["four"] = "4",
+                ["Two"] = "-2-"
+            };
+
+            var result = baseStore.MergeIgnoreCase(newStore);
+
+            result.Count.ShouldBe(4);
+            result["one"].ShouldBe("1");
+            result["Two"].ShouldBe("-2-");
+            result["three"].ShouldBe("3");
+            result["four"].ShouldBe("4");
+        }
+
+        [Fact]
+        public void MergeIgnoreCaseTestsWhenIgnoreExistingIsTrue()
+        {
+            var baseStore = new Dictionary<string, string>
+            {
+                ["one"] = "1",
+                ["two"] = "2",
+                ["three"] = "3"
+            };
+
+            var newStore = new Dictionary<string, string>
+            {
+                ["four"] = "4",
+                ["Two"] = "-2-"
+            };
+
+            var result = baseStore.MergeIgnoreCase(newStore, true);
+
+            result.Count.ShouldBe(4);
+            result["one"].ShouldBe("1");
+            result["two"].ShouldBe("2");
+            result["three"].ShouldBe("3");
+            result["four"].ShouldBe("4");
+        }
+
+        [Fact]
+        public void AppendTests()
+        {
+            var source = new Dictionary<string, string>
+            {
+                ["one"] = "1",
+                ["two"] = "2",
+                ["three"] = "3"
+            };
+
+            var newSource = new Dictionary<string, string>
+            {
+                ["two"] = "-2-",
+                ["four"] = "4"
+            };
+
+            _ = source.Append(newSource);
+
+            source.Count.ShouldBe(4);
+            source["one"].ShouldBe("1");
+            source["two"].ShouldBe("-2-");
+            source["three"].ShouldBe("3");
+            source["four"].ShouldBe("4");
+        }
+
+        [Fact]
+        public void AppendTestsWhenIgnoreExistingKey()
+        {
+            var source = new Dictionary<string, string>
+            {
+                ["one"] = "1",
+                ["two"] = "2",
+                ["three"] = "3"
+            };
+
+            var newSource = new Dictionary<string, string>
+            {
+                ["two"] = "-2-",
+                ["four"] = "4"
+            };
+
+            _ = source.Append(newSource, true);
+
+            source.Count.ShouldBe(4);
+            source["one"].ShouldBe("1");
+            source["two"].ShouldBe("2");
+            source["three"].ShouldBe("3");
+            source["four"].ShouldBe("4");
+        }
     }
 }
--- src/Bolt.Common.Extensions/DictionaryExtensions.cs
+++ src/Bolt.Common.Extensions/DictionaryExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace Bolt.Common.Extensions
@@ -19,6 +20,15 @@ namespace Bolt.Common.Extensions
             return source.TryGetValue(key, out result) ? result : defaultValue;
         }
 
+        /// <summary>
+        /// Merge a dictionary with another dictionary and return a new dictionary
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="mergeWith"></param>
+        /// <param name="ignoreWhenAlreadyExists"></param>
+        /// <returns></returns>
         [DebuggerStepThrough]
         public static IDictionary<TKey, TValue> Merge<TKey, TValue>(this IDictionary<TKey, TValue> source, 
             IDictionary<TKey,TValue> mergeWith,
@@ -45,6 +55,73 @@ namespace Bolt.Common.Extensions
             }
 
             return result;
+        }
+
+        /// <summary>
+        /// Merge two dictionary into a new dictionary where keys are case insensitive and return the new dictionary. 
+        /// Please note, the final dictionary that return will compare key in case insensitive way
+        /// </summary>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="mergeWith"></param>
+        /// <param name="ignoreWhenAlreadyExists"></param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static IDictionary<string, TValue> MergeIgnoreCase<TValue>(this IDictionary<string, TValue> source,
+            IDictionary<string, TValue> mergeWith,
+            bool ignoreWhenAlreadyExists = false)
+        {
+            if (source == null) return mergeWith;
+            if (mergeWith == null) return source;
+
+            var result = new Dictionary<string, TValue>(StringComparer.OrdinalIgnoreCase);
+
+            foreach (var keyVal in source)
+            {
+                result[keyVal.Key] = keyVal.Value;
+            }
+
+            foreach (var keyVal in mergeWith)
+            {
+                if (ignoreWhenAlreadyExists)
+                {
+                    if (result.ContainsKey(keyVal.Key)) continue;
+                }
+
+                result[keyVal.Key] = keyVal.Value;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Append a dictionary to the source dictionary and return the source dictionary
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="mergeWith"></param>
+        /// <param name="ignoreWhenAlreadyExists"></param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static IDictionary<TKey, TValue> Append<TKey, TValue>(this IDictionary<TKey, TValue> source,
+            IDictionary<TKey, TValue> mergeWith,
+            bool ignoreWhenAlreadyExists = false)
+        {
+            if (source == null) return mergeWith;
+            if (mergeWith == null) return source;
+
+            foreach (var keyVal in mergeWith)
+            {
+                if (ignoreWhenAlreadyExists)
+                {
+                    if (source.ContainsKey(keyVal.Key)) continue;
+                }
+
+                source[keyVal.Key] = keyVal.Value;
+            }
+
+            return source;
         }
     }
 }

dcc17fe1e3a61fffade3615c8ad609636c0d75e4
Mohammad Ruhul Amin <mruhul@gmail.com>
2023-12-29 01:47:20 +1100

    Add two more extension methods to convert string to memory stream and vice versa

---
+++ src/Bolt.Common.Extensions/StreamExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Bolt.Common.Extensions;
+
+public static class StreamExtensions
+{
+    public static async Task<string> ReadAsString(this Stream ms, CancellationToken ct)
+    {
+        using var sr = new StreamReader(ms);
+        return await sr.ReadToEndAsync(ct);
+    }
+
+    public static MemoryStream ToMemoryStream(this string source)
+    {
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+        writer.Write(source);
+        writer.Flush();
+        stream.Position = 0;
+        return stream;
+    }
+}

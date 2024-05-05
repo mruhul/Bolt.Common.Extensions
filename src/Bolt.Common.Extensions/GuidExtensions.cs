12f6ef179f2a2d29d11bb11c9ce3f91aaedd4995
suman-carsales <suman.kumarnath@carsales.com.au>
2024-05-06 00:00:03 +0530

    nullable string for toGuid method

--- src/Bolt.Common.Extensions/GuidExtensions.cs
+++ src/Bolt.Common.Extensions/GuidExtensions.cs
@@ -6,10 +6,9 @@ namespace Bolt.Common.Extensions
     public static class GuidExtensions
     {
         [DebuggerStepThrough]
-        public static Guid? ToGuid(this string source)
+        public static Guid? ToGuid(this string? source)
         {
-            Guid result;
-            return Guid.TryParse(source, out result) ? result : (Guid?)null;
+            return Guid.TryParse(source, out var result) ? result : null;
         }
 
         [DebuggerStepThrough]
